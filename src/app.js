c51b1e7ed2cfe64c73c0eaaa3d495c0ddbdfc8e0
Fred-Bell <fwgbell@gmail.com>
2018-12-05 15:34:00 +0000

    Front end file structure

--- src/app.js
+++ src/app.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import './scss/style.scss';
+import 'bulma';
+
+import Header from './components/Header';
+import Home from './components/Home';
+import AuthLogin from './components/auth/Login';
+import AuthRegister from './components/auth/Register';
+import PitchIndex from './coomoponents/pitches/Index';
+import PitchShow from './coomoponents/pitches/Show';
+import PitchNew from './coomoponents/pitches/New';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <main className="container">
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/pitches' component={PitchIndex} />
+              <Route exact path='/pitches/new' component={PitchNew} />
+              <Route path='/pitches/:id' component={PitchShow}/>
+              <Route path="/login" component={AuthLogin} />
+              <Route path="/register" component={AuthRegister} />
+            </Switch>
+          </main>
+        </div>
+      </BrowserRouter>
     );
   }
 }
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
