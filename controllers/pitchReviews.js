a0f439185e60c45b1dd4118346c716d84f9b1d7b
Fred-Bell <fwgbell@gmail.com>
2018-12-06 15:27:08 +0000

    made review update and delete routes in backend

--- controllers/pitchReviews.js
+++ controllers/pitchReviews.js
@@ -4,7 +4,7 @@ function createRoute(req, res, next){
   req.body.reviewedBy = req.currentUser._id;
   Pitch
     .findById(req.params.id)
-    .populate('reviews.user')
+    .populate('uploadedBy reviews.reviewedBy')
     .then(pitch => {
       pitch.reviews.push(req.body);
       return pitch.save();
@@ -14,11 +14,35 @@ function createRoute(req, res, next){
 }
 
 function updateRoute(req, res, next){
-
+  Pitch
+    .findById(req.params.id)
+    .populate('uploadedBy reviews.reviewedBy')
+    .then(pitch => {
+      const review = pitch.reviews.id(req.params.reviewId);
+      if(!review.reviewedBy._id.equals(req.currentUser._id)) {
+        throw new Error('Unauthorized');
+      }
+      review.set(req.body);
+      return pitch.save();
+    })
+    .then(pitch => res.json(pitch))
+    .catch(next);
 }
 
 function deleteRoute(req, res, next){
-
+  Pitch
+    .findById(req.params.id)
+    .populate('uploadedBy reviews.reviewedBy')
+    .then(pitch => {
+      const review = pitch.reviews.id(req.params.reviewId);
+      if(!review.reviewedBy._id.equals(req.currentUser._id)) {
+        throw new Error('Unauthorized');
+      }
+      review.remove();
+      return pitch.save();
+    })
+    .then(pitch => res.json(pitch))
+    .catch(next);
 }
 
 module.exports = {
