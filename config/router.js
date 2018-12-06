04b59603212013647e5ddaa0dedd148c2e0483a5
Fred-Bell <fwgbell@gmail.com>
2018-12-06 14:31:31 +0000

    added review routes to router

--- config/router.js
+++ config/router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const pitches = require('../controllers/pitches');
 const auth = require('../controllers/auth');
+const pitchReviews = require('../controllers/pitchReviews');
 const secureRoute = require('../lib/secureRoute');
 
 router.route('/pitches')
@@ -11,6 +12,13 @@ router.route('/pitches/:id')
   .get(pitches.show)
   .put(secureRoute, pitches.update)
   .delete(secureRoute, pitches.delete);
+
+router.route('/pitches/:id/reviews')
+  .post(secureRoute, pitchReviews.create);
+
+router.route('/pitches/:id/reviews/:reviewId')
+  .put(secureRoute, pitchReviews.update)
+  .delete(secureRoute, pitchReviews.delete);
 
 router.post('/register', auth.register);
 router.post('/login', auth.login);
