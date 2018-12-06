550ec64af9c6785019acfa69a4c7c6d8242da79d
Fred-Bell <fwgbell@gmail.com>
2018-12-06 15:40:16 +0000

    added delete review function to show component

--- src/components/pitches/Show.js
+++ src/components/pitches/Show.js
@@ -9,7 +9,7 @@ class PitchShow extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.handleDelete = this.handleDelete.bind(this);
+    this.deletePitch = this.deletePitch.bind(this);
   }
 
   componentDidMount() {
@@ -17,10 +17,14 @@ class PitchShow extends React.Component {
       .then(result => this.setState({ pitch: result.data }));
   }
 
-  handleDelete(event){
-    event.preventDefault();
+  deletePitch(){
     axios.delete(`/api/pitches/${this.state.pitch._id}`, authorizationHeader())
       .then( () => this.props.history.push('/pitches'));
+  }
+
+  deleteReview = review =>{
+    axios.delete(`/api/pitches/${this.state.pitch._id}/reviews/${review._id}`, authorizationHeader())
+      .then(result => this.setState({ pitch: result.data }));
   }
 
   render() {
@@ -45,7 +49,7 @@ class PitchShow extends React.Component {
                 {pitch.uploadedBy._id === decodeToken().sub &&
                   <div className="column is-6 buttonWrapperShowPage">
                     <Link to={`/pitches/${pitch._id}/edit`}><button className="button is-rounded">Edit</button></Link>
-                    <button onClick={this.handleDelete} className="button is-rounded is-danger">Delete</button>
+                    <button onClick={this.deletePitch} className="button is-rounded is-danger">Delete</button>
                   </div>
                 }
               </div>
@@ -65,7 +69,7 @@ class PitchShow extends React.Component {
                     {review.reviewedBy._id === decodeToken().sub &&
                       <div>
                         <button className="button is-rounded">Edit</button>
-                        <button className="button is-rounded is-danger">Delete</button>
+                        <button onClick={() => this.deleteReview(review)} className="button is-rounded is-danger">Delete</button>
                       </div>
                     }
                   </div>)
