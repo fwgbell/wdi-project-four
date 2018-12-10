0d6f6ee96258ca644893b12df4e8198d06e8ffe9
Fred-Bell <fwgbell@gmail.com>
2018-12-10 12:05:12 +0000

    added display for distance on index page

--- src/components/pitches/Index.js
+++ src/components/pitches/Index.js
@@ -7,7 +7,8 @@ class PitchIndex extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      titleText: 'Highest Scoring Pitches'
+      titleText: 'Highest Scoring Pitches',
+      filter: 'score'
     };
     this.sortByDistance = this.sortByDistance.bind(this);
     this.sortByScore = this.sortByScore.bind(this);
@@ -23,7 +24,7 @@ class PitchIndex extends React.Component {
       pitches.sort(function(a, b){
         return a.distance - b.distance;
       });
-      this.setState({ titleText: 'Closest Pitches To You', pitches: pitches});
+      this.setState({ titleText: 'Closest Pitches To You', filter: 'distance', pitches: pitches});
     }, this.setState({ titleText: 'Looking For Your Location...'}));
   }
 
@@ -31,7 +32,7 @@ class PitchIndex extends React.Component {
     const pitches = this.state.pitches.sort(function(a, b){
       return b.averageRating - a.averageRating;
     });
-    this.setState({ titleText: 'Highest Scoring Pitches', pitches: pitches});
+    this.setState({ titleText: 'Highest Scoring Pitches', filter: 'score', pitches: pitches});
   }
 
   componentDidMount() {
@@ -55,7 +56,7 @@ class PitchIndex extends React.Component {
         <h1 className="title column is-12">{this.state.titleText}</h1>
         {this.state.pitches
           ?
-          this.state.pitches.map(pitch => <PitchWrapper key={pitch._id} pitch={pitch}/>)
+          this.state.pitches.map(pitch => <PitchWrapper key={pitch._id} pitch={pitch} filter={this.state.filter}/>)
           :
           <p>Loading...</p>}
       </section>
--- src/components/pitches/PitchWrapper.js
+++ src/components/pitches/PitchWrapper.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-function PitchWrapper({ pitch }){
+function PitchWrapper({ pitch, filter }){
   return (
     <Link className="column is-6 pitchWrapper" to={`/pitches/${pitch._id}`}>
-      {pitch.averageRating ?
+      {pitch.averageRating && filter === 'score'?
         <h2>{pitch.name} - {pitch.averageRating}/5</h2>
         :
-        <h2>{pitch.name}</h2>
+        pitch.distance && filter === 'distance'?
+          <h2>{pitch.name} - {pitch.distance.toFixed(2)} Km away</h2>
+          :
+          <h2>{pitch.name}</h2>
       }
       <img src={pitch.image}/>
     </Link>
